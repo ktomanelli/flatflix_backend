3dd6fd784c7248e2d892c4ba774d56ac1fb25ed2
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-07-07 12:41:28 -0400

    added routes and methods for video index/show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,13 @@
 class VideosController < ApplicationController
+    
+    def index
+        @movies = Video.all
+        render json: @movies
+    end
+
+    def show
+        @movie = Video.find(params[:id])
+        render json: @movie
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_videos
-  resources :users
-  resources :video_genres
-  resources :genres
-  resources :videos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/videos', to: 'videos#index'
+  get '/videos/:id', to: 'videos#show'
 end
